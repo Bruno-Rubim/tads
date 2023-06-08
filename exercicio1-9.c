183c4fab938cc734433bd8add6a74e30de9f68d4
Bruno Rubim <127158283+Bruno-Rubim@users.noreply.github.com>
2023-06-07 21:55:34 -0300

    Add files via upload

--- exercicio1-9.c
+++ exercicio1-9.c
@@ -1,34 +1,37 @@
 #include <stdio.h>
 
 int main(){
-    
-    int tam = 8, m[tam][tam], resp, maior = -99999, maiorI, maiorJ, menor = 99999, menorI, menorJ;
+	
+	int m, n;
+	
+	printf("valor de m: ");
+	scanf("%d", &m);
+	printf("valor de n: ");
+	scanf("%d", &n);
+	
+	int a[m][n], b[m][n], c[m][n];
+	
+	for(int i = 0; i<n; i++){
+		for(int j = 0; j<m; j++){
+			printf("valor de a[%d][%d]: ", i, j);
+			scanf("%d", &a[i][j]);
+		}
+	}
 
-    for (int i = 0; i < tam; ++i){
-        for (int j = 0; j < tam; ++j){
-            printf("manda um numero: ");
-            scanf("%d", &resp);
-            m[i][j] = resp;
-            if (resp > maior){
-                maior = resp;
-                maiorI = i;
-                maiorJ = j;
-            }
-            if (resp < menor){
-                menor = resp;
-                menorI = i;
-                menorJ = j;
-            }
-        }
-    }
-    for (int i = 0; i < tam; ++i){
-		for (int j = 0; j < tam; ++j){
-			printf("%d ", m[i][j]);
+	for(int i = 0; i<n; i++){
+		for(int j = 0; j<m; j++){
+			printf("valor de b[%d][%d]: ", i, j);
+			scanf("%d", &b[i][j]);
+		}
+	}
+
+	for(int i = 0; i<n; i++){
+		for(int j = 0; j<m; j++){
+			c[i][j] = a[i][j] + b[i][j];
+			printf("%d\t", c[i][j]);
 		}
 		printf("\n");
 	}
-    printf("Maior: %d[%d][%d]", maior, maiorI, maiorJ);
-    printf("Menor: %d[%d][%d]", menor, menorI, menorJ);
-
-    return 0;
+	
+	return 0;
 }
